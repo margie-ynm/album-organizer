1e2ed5fce4fd8f0b333051c62b7c491c53ab5308
Jim Padilla and Margie Nevarez <margie@>
2016-11-30 15:29:39 -0800

    Add add_album, cds methods and specs

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,8 @@ class Artist
 
   define_method(:initialize) do |name|
     @name = name
-    # @id = @@artists.length().+(1)
+    @id = @@artists.length().+(1)
+    @cds = []
   end
 
   define_method(:name) do
@@ -16,6 +17,32 @@ class Artist
 
   define_singleton_method(:all) do
     @@artists
+  end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_method(:cds) do
+    @cds
+  end
+
+  define_method(:add_album) do |album|
+    @cds.push(album)
+  end
+
+  define_singleton_method(:clear) do
+    @@artists = []
+  end
+
+  define_singleton_method(:find_it) do |identification|
+    found_artist = nil
+    @@artists.each do |artist|
+      if artist.id().eql?(identification.to_i)
+        found_artist = artist
+      end
+    end
+    found_artist
   end
 
 end
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -4,6 +4,9 @@ require('artist')
 
 
 describe(Artist) do
+  before() do
+    Artist.clear()
+  end
 
   describe('#name') do
     it("returns name of Artist") do
@@ -18,11 +21,55 @@ describe(Artist) do
     end
   end
 
+  describe('#cds') do
+    it("initially returns an empty array of cds for the artist") do
+      test_artist = Artist.new("Kendrick Lamar")
+      expect(test_artist.cds()).to(eq([]))
+    end
+  end
+
   describe('#save') do
     it('saves the artist to an array of artists') do
       test_artist = Artist.new("Beach House")
       test_artist.save()
       expect(Artist.all()).to(eq([test_artist]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved Artists") do
+      Artist.new("Foster the People").save()
+      Artist.clear()
+      expect(Artist.all()).to(eq([]))
+    end
+  end
+
+  describe("#id") do
+    it("returns the id for the artist") do
+      test_artist = Artist.new("Rae Sremmurd")
+      test_artist.save()
+      test_artist2 = Artist.new("The Internet")
+      test_artist2.save()
+      expect(test_artist2.id()).to(eq(2))
+    end
+  end
+
+  describe(".find_it") do
+    it("returns an artist by its id number") do
+      test_artist = Artist.new("Mac Miller")
+      test_artist.save()
+      test_artist2 = Artist.new("A Tribe Called Quest")
+      test_artist2.save()
+      expect(Artist.find_it(test_artist.id())).to(eq(test_artist))
+    end
+  end
+
+  describe('#add_album') do
+    it('adds an album to an artist') do
+      test_album = Album.new("Best Day Ever", 2011)
+      test_artist = Artist.new("Mac Miller")
+      test_artist.add_album(test_album)
+      expect(test_artist.cds()).to(eq([test_album]))
     end
   end
 
