1dd78e560c283f3453b13858c08c23bd52035f9c
Jim Padilla and Margie Nevarez <jim@>
2016-11-30 14:58:39 -0800

    Add save and all methods/specs

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,21 @@
 class Artist
+  @@artists = []
+
   define_method(:initialize) do |name|
     @name = name
-
+    # @id = @@artists.length().+(1)
   end
 
   define_method(:name) do
     @name
   end
+
+  define_method(:save) do
+    @@artists.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@artists
+  end
+
 end
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -5,9 +5,25 @@ require('artist')
 
 describe(Artist) do
 
-  it("returns name of Artist") do
-    test_artist = Artist.new("The Strokes")
-    expect(test_artist.name()).to(eq("The Strokes"))
+  describe('#name') do
+    it("returns name of Artist") do
+      test_artist = Artist.new("The Strokes")
+      expect(test_artist.name()).to(eq("The Strokes"))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Artist.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves the artist to an array of artists') do
+      test_artist = Artist.new("Beach House")
+      test_artist.save()
+      expect(Artist.all()).to(eq([test_artist]))
+    end
   end
 
 
