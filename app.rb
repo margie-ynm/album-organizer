82d7241f37a87042b526eb3209864a7a6f55f326
Jim Padilla and Margie Nevarez <margie@>
2016-11-30 16:48:59 -0800

    create routing

--- app.rb
+++ app.rb
@@ -4,21 +4,64 @@ also_reload('lib/**/*.rb')
 require('./lib/album')
 require('./lib/artist')
 
+# Home
 get('/') do
   erb(:index)
 end
 
+# Add new artist page
 get('/artist/new') do
   erb(:new_artist)
 end
 
+# Takes user input, adds it to artist array
 post('/new_artist') do
   name = params.fetch('artist')
   Artist.new(name).save()
   erb(:success)
 end
 
+# Shows all artists
 get('/artists/all') do
   @artists = Artist.all()
   erb(:all_artists)
 end
+
+# Add new album page
+get('/album/new') do
+  erb(:new_album)
+end
+
+# Takes user input, adds it to the album array
+post('/new_album') do
+  album_name = params.fetch('album')
+  year_date = params.fetch('year')
+  Album.new(album_name, year_date).save()
+  erb(:success)
+end
+
+# Show all albums
+get('/albums/all') do
+  @albums = Album.all()
+  erb(:all_albums)
+end
+
+# Dynamic pages for artists
+get('/artists/:id') do
+  @album = Album.find(params.fetch('id').to_i())
+  erb(:artist)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
